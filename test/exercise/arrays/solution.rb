846d8ae418e7303f675fd24786572167a16494bd
Dmitry Barskov <dmitry.barskov@flatstack.com>
2022-02-09 08:35:23 +0300

    Solve arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,24 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_value = array.max
+        array.map { |item| item > 0 ? max_value : item }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query, lo: 0, hi: array.length)
+        return -1 if (hi - lo).zero?
+        return lo if hi - lo == 1 && query == array[lo]
+        return -1 if hi - lo == 1
+
+        me = (lo + hi) / 2
+
+        if query == array[me]
+          me
+        elsif query < array[me]
+          search(array, query, lo: lo, hi: me)
+        else
+          search(array, query, lo: me, hi: hi)
+        end
       end
     end
   end
